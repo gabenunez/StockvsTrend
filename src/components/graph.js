0539116096df767b9ebb932241128bb0f68fbfb2
Gabe Nuñez <gabe@gabenunez.com>
2018-07-31 12:52:25 -0700

    Initial graph component creation using recharts

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-class Graph extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Graph here</h2>
-            </div>
-        );
+const Graph = (props) => {
+
+    console.log(props.stockList);
+    if(!props.stockList) {
+        return <div>Loading...</div>;
     }
+
+    return (
+        <ResponsiveContainer width='100%' height={300}>
+            <LineChart data={props.stockList}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <Tooltip/>
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+        </ResponsiveContainer>
+    );
 }
 
 export default Graph;
