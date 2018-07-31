f99564c0462d83c382719f6122d50323182a46d0
Gabe Nuñez <gabe@gabenunez.com>
2018-07-31 00:31:04 -0700

    Remove generics and add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Stocks vs Trends</h1>
+          </div>
+        </div>
       </div>
     );
   }
