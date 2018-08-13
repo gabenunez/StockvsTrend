d25b2f064e1ed429c866886deb3ca89d4e1f238e
Gabe Nuñez <gabe@gabenunez.com>
2018-08-12 18:48:40 -0700

    Uppercase all ticker form inputs

--- client/src/App.js
+++ client/src/App.js
@@ -53,8 +53,13 @@ class App extends Component {
 
   handleInputChange(event) {
     const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
+
+    let value = target.type === 'checkbox' ? target.checked : target.value;
+
+    if (name === 'tickerSymbol') {
+      value = value.toLocaleUpperCase();
+    }
 
     this.setState({
       [name]: value
