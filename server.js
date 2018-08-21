d073dfeb81cc38a8c9eabf25b23cb1cb37cee36b
Gabe Nuñez <gabe@gabenunez.com>
2018-08-20 21:03:35 -0700

    Updated with new build commands

--- server.js
+++ server.js
@@ -75,4 +75,13 @@ app.get('/api/stocks', (req, res) => {
     }
 });
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
